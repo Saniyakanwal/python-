2499c3e22deb5278e17628c9ac780b5f9b9f64e7
Saniyakanwal <160118062+Saniyakanwal@users.noreply.github.com>
2025-02-25 15:20:14 +0500

    lecture 1

---
+++ Lecture-1-variables-and-datatypes/lecture1.py
@@ -0,0 +1,100 @@
+# print("saniya kanwal")
+# print("my age is 20")
+
+# # numbers
+
+# print(23)
+# print(5)
+
+# # variable = memory
+
+# name= "saniya kanwal" #string
+# age = 20  #number
+
+# print("my name is:",name)
+# print("my age is:",age)
+
+# # Data Types
+# # 1. Integers
+# num = -25
+# print(type(num))
+
+# # 2. Float
+# num = 25.5
+# print(type(num))
+
+# # 3. String
+# name = "kanwal"
+# print(type(name))
+
+# # 4. Boolean
+# is_admin = True
+# print(type(is_admin))
+
+# # 5. None
+# a= None
+# print(type(a))
+
+
+# # sum
+# a = 1000
+# b = 500
+# sum = a + b
+# print(sum)
+
+
+
+# # arithmetic operator
+# a = 5
+# b = 2
+
+# print(a+b)  #addition
+# print(a-b)  #subtraction
+# print(a*b)  #multiply
+# print(a/b)  #divide
+# print(a%b)  #modulus
+# print(a**b)  #exponentiation
+
+# # Relational / comparison operator
+# a = 50
+# b = 20
+
+# print(a == b)
+# print(a != b)
+# print(a > b) #greater
+# print(a < b) #lessthen
+# print(a <= b)
+# print(a >= b)
+
+# assigment operator
+# num = 10
+# num += 10
+# num -= 10
+# num *= 10
+# num /= 10
+# num %= 10
+# num **= 5
+# print(num)
+
+# logical opperator
+
+# print(not False)
+# print(not True)
+
+# val1 = False
+# val2 = True
+# print(val1 and val2)
+
+# print(val1 or val2)
+  
+
+#   type conversion
+# a= 2
+# b= 4.25
+
+# print(a +b )
+
+
+# input in python
+
+# input("enter your name:")
---
+++ Lecture-1-variables-and-datatypes/practice.py
@@ -0,0 +1,30 @@
+# question 1
+# Write a Program to input 2 numbers & print their sum.
+first = int(input("enter first num: "))
+second = int(input("enter second num: "))
+
+print(first + second)
+
+
+# question 2
+# WAP to input side of a square & print its area.
+# int
+side = int(input("enter side of square:"))
+print("area = " , side * side)
+
+# float
+side = float(input("enter side of square:"))
+print("area = " , side * side)
+
+# question 3
+# WAP to input 2 floating point numbers & print their average.
+a = float(input("enter first num :"))
+b = float(input("enter second num :"))
+print("avg = ",(a+b)/2 )
+
+# question 4
+# WAP to input 2 int numbers, a and b. Print True if a is greater than or equal to b. If not print False.
+a = int(input("enter first num :"))
+b = int(input("enter second num :"))
+
+print(a >= b )
